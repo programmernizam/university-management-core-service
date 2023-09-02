55d9ca5e4116c53c3db0d7fb9c020f705c8d6aac
programmernizam <mdnizam12109@gmail.com>
2023-09-02 22:51:41 +0600

    Student update api validation created

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { FacultyController } from './faculty.controller';
+import { FacultyValidation } from './faculty.validation';
 
 const router = express.Router();
 
 router.get('/', FacultyController.getAllData);
 router.get('/:id', FacultyController.getSingleData);
-router.post('/', FacultyController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(FacultyValidation.create),
+  FacultyController.insertIntoDB,
+);
 
 export const FacultyRoutes = router;
--- src/app/modules/students/student.routes.ts
+++ src/app/modules/students/student.routes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { StudentController } from './student.controller';
+import { StudentValidation } from './student.validation';
 const router = express.Router();
 
 router.get('/', StudentController.getAllData);
 router.get('/:id', StudentController.getSingleData);
-router.post('/', StudentController.insertIntoDB);
-router.patch('/:id', StudentController.updateIntoDB);
+router.post(
+  '/',
+  validateRequest(StudentValidation.create),
+  StudentController.insertIntoDB,
+);
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.update),
+  StudentController.updateIntoDB,
+);
 
 export const StudentRoutes = router;
