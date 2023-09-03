b3b52a4640770da435da8a31e04c6ac088ec14fe
programmerNizam <mdnizam12109@gmail.com>
2023-09-03 22:29:36 +0600

    Building add wrap by auth users

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
 import { BuildingValidation } from './building.validation';
@@ -8,10 +10,20 @@ router.get('/', BuildingController.getAllData);
 router.get('/:id', BuildingController.getSingleData);
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(BuildingValidation.create),
   BuildingController.insertIntoDB,
 );
-router.patch('/:id', BuildingController.updateIntoDB);
-router.delete('/:id', BuildingController.deleteIntoDB);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(BuildingValidation.update),
+  BuildingController.updateIntoDB,
+);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  BuildingController.deleteIntoDB,
+);
 
 export const BuildingRoutes = router;
